f34fc549fde594d6d1c614b50dad8852c32c9080
abenidek <abenezerd422@gmail.com>
2025-11-08 15:15:27 +0300

    added physics body intializations

--- body.go
+++ body.go
@@ -1,9 +1,11 @@
 package phygo
 
+import "math"
+
 type ShapeType int
 
 const (
-	CircleShape = iota
+	CircleShape ShapeType = iota
 	RectangleShape
 )
 
@@ -24,4 +26,37 @@ type Body struct {
 	// used for rectangle shapes
 	Width  float32
 	Height float32
+}
+
+func CreateBodyCircle(pos Vector, isStatic bool, radius, density float32) *Body {
+	newBody := &Body{
+		Position:        pos,
+		Velocity:        Vector{},
+		AngularVelocity: 0.0,
+		Restitution:     0.0,
+		IsStatic:        false,
+		Radius:          radius,
+		ShapeType:       CircleShape,
+	}
+	newBody.Area = radius * radius * math.Pi
+	newBody.Mass = newBody.Area * density
+
+	return newBody
+}
+
+func CreateBodyRectangle(pos Vector, isStatic bool, width, height, density float32) *Body {
+	newBody := &Body{
+		Position:        pos,
+		Velocity:        Vector{},
+		AngularVelocity: 0.0,
+		Restitution:     0.0,
+		IsStatic:        false,
+		Width:           width,
+		Height:          height,
+		ShapeType:       RectangleShape,
+	}
+	newBody.Area = height * width
+	newBody.Mass = newBody.Area * density
+
+	return newBody
 }
